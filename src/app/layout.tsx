230d47801681edbd506c9a3cd333d0b7fcf43b20
Rick <ricleyreis@gmail.com>
2025-09-01 20:48:44 -0300

    refactor: adicionando fonte Saira e ajustando fonte Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from "next";
 import { Inter} from "next/font/google";
+import { Saira } from "next/font/google";
 import Header from "@/components/Header";
+import "../styles/global.css";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const saira = Saira({
+  variable: "--font-saira",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -18,8 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.variable}>
+    <html lang="pt-BR" className={inter.variable}>
+      <body className={saira.variable}>
         <Header />
         <main>{children}</main>
       </body>
