660c8cc466d4b823b63d0d5e415e8c16e213bd48
Rick <ricleyreis@gmail.com>
2025-09-03 23:44:49 -0300

    feat: inciando página dinamica de produto

---
+++ src/app/produto/[id]/page.tsx
@@ -0,0 +1,157 @@
+
+import { getCategories, getProduct } from "@/services/api";
+import { notFound } from "next/navigation";
+import SortSelect from "@/components/SortSelect";
+import { AddToCartButton } from "@/components/AddToCartButton";
+import { CategoryNamePriceWrapper, DescriptionWrapper, Image } from "./style";
+import { priceToBRL } from "@/utils/formatting";
+
+type ProductFromApi = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+  description: string;
+  rating: number;
+  stock: number;
+  brand: string;
+};
+
+type Category = {
+  id: string;
+  name: string;
+  description: string;
+  icon: string;
+  productCount: number;
+};
+
+type ProductPageProps = {
+  params: { id: number };
+};
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { id } = params;
+
+  let product: ProductFromApi | null = null;
+  let categoryArray: Category[] = [];
+  let categoriesMap: Record<string, Category> = {};
+  let productCategory: Category | null = null;
+
+  try {
+    const cats = await getCategories();
+    categoryArray = cats.categories;
+
+    categoriesMap = categoryArray.reduce(
+      (acc: Record<string, Category>, cat: Category) => {
+        acc[cat.id] = cat;
+        return acc;
+      },
+      {} as Record<string, Category>
+    );
+
+    const data = await getProduct(id);
+
+    product = data.product || data;
+
+    if (product) {
+      productCategory = categoriesMap[product.category] || null;
+    }
+  } catch (error) {
+    console.error(error);
+    notFound();
+  }
+
+  return (
+    <main
+      style={{
+        padding: "7rem 10rem",
+        width: "100%",
+        boxSizing: "border-box",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        Voltar
+        <SortSelect />
+      </div>
+
+      <div
+        style={{
+          paddingTop: "2.5rem",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            marginTop: "2rem",
+            justifyItems: "center",
+            alignItems: "center",
+          }}
+        >
+          {product && (
+            <div
+              style={{
+                display: "flex",
+                justifyItems: "center",
+                alignItems: "center",
+                gap: "1rem",
+                fontFamily: "Inter, sans-serif"
+              }}
+            >
+              <Image src={product.image} alt={product.name} />
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyItems: "center",
+                  alignItems: "center",
+                }}
+              >
+                <CategoryNamePriceWrapper>
+                  <span>{productCategory?.name}</span>
+                  <h2>{product.name}</h2>
+                  <h3
+                    style={{
+                      fontSize: "1.25rem",
+                      fontWeight: "600",
+                      
+                    }}
+                  >
+                    {priceToBRL(product.price)}
+                  </h3>
+                </CategoryNamePriceWrapper>
+
+                <DescriptionWrapper>
+                  <h3 style={{
+                    fontSize: "1rem",
+                    color: "#737380",
+                    fontWeight: "500",
+                    textTransform: "uppercase"
+                  }}>Descrição</h3>
+                  <p
+                    style={{
+                      fontSize: "0.9rem",
+                      fontWeight: "400",
+                      color: "#41414D",
+                    }}
+                  >
+                    {product.description}
+                  </p>
+                </DescriptionWrapper>
+
+                <AddToCartButton />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
+  );
+}
