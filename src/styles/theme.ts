cf2010ca71bdef40d8d303b81628406a44f7d571
Rick <ricleyreis@gmail.com>
2025-09-05 17:36:21 -0300

    refactor: ajustar arquivo de theme com breakpoints para styled-components

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,13 @@
-export const breakpoints = {
+const size = {
   mobile: "480px",
   tablet: "768px",
   laptop: "1024px",
   desktop: "1440px",
+};
+
+export const device = {
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  desktop: `(max-width: ${size.desktop})`,
 };
