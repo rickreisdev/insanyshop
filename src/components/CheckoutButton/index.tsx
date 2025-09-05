08ea5bd83188e98cdbf999a90b959a3b8a383e4a
Rick <ricleyreis@gmail.com>
2025-09-05 17:16:50 -0300

    feat: adicionar ação de limpar carrinho e redirecionar para home no botão de checkout

--- src/components/CheckoutButton/index.tsx
+++ src/components/CheckoutButton/index.tsx
@@ -1,12 +1,21 @@
-// "use client";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 import { Button, ButtonBox } from "./styles";
 import { useCart } from "@/hooks/useCart";
 
 export function CheckoutButton() {
+  const router = useRouter();
+  const {clearCart} = useCart();
+
+  function handleCheckout(){
+    clearCart()
+    router.replace("/")
+  }
 
   return (
-    <ButtonBox>
+    <ButtonBox onClick={handleCheckout}>
       <Button>
         Finalizar a compra
       </Button>
