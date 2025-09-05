f937ad2bd42c9b5906580de7aeda9d5907e72bc6
Rick <ricleyreis@gmail.com>
2025-09-04 23:10:55 -0300

    refactor: ajustando layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import { Saira } from "next/font/google";
 import Header from "@/components/Header";
 import "../styles/global.css";
