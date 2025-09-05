28793e640370be3ac3e2ea1654e1150cccf4b6bf
Rick <ricleyreis@gmail.com>
2025-09-04 23:17:53 -0300

    feat: botão de finalizar compra

---
+++ src/components/CheckoutButton/index.tsx
@@ -0,0 +1,22 @@
+// "use client";
+
+import { Button, ButtonBox } from "./styles";
+
+// type CheckoutProps = {
+//   id: number;
+//   name: string;
+//   price: number;
+//   image: string;
+//   stock: number;
+// };
+
+export function CheckoutButton() {
+
+  return (
+    <ButtonBox>
+      <Button>
+        Finalizar a compra
+      </Button>
+    </ButtonBox>
+  );
+}
---
+++ src/components/CheckoutButton/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000;
+  height: 2.5rem;
+  border-radius: 4px;
+  width: 100%;
+  color: #ffffff;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+
+  &:hover {
+    background-color: #141414ff;
+  }
+`;
